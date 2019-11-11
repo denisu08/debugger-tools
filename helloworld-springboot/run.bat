19bc47bf27a54d581dd7067d3db2b2d81afbd257
Denis Prasetio <denisu08@gmail.com>
2019-11-11 16:23:11 +0700

    Add helloworld springboot example for testing purpose

--- helloworld-springboot/run.bat
+++ helloworld-springboot/run.bat
@@ -1,1 +1,1 @@
-java -Xdebug -Xrunjdwp:transport=dt_socket,address=7777,server=y,suspend=n -jar build/libs/helloworld-1.0-SNAPSHOT.jar
+java -Xdebug -Xrunjdwp:transport=dt_socket,address=7777,server=y,suspend=n -jar build/libs/gs-spring-boot-0.1.0.jar
